d1f94f10dbadb48433c39a55ccc3f5a0572b0edc
Nelson Foltz <nelsfoltz@gmail.com>
2020-09-27 10:18:16 -0400

    add: fontawesome dependency

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+function App() {
+  return (
+    <div>
+      <i className="fas fa-home fa-7x" />
+    </div>
+  );
+}
+
+export default App;
