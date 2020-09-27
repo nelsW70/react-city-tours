5b6ea83d3bc245bcda48cd6b1ab8d9d2be5922c3
Nelson Foltz <nelsfoltz@gmail.com>
2020-09-27 15:39:12 -0400

    add: built out Tour component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import Navbar from './components/Navbar/Navbar';
 import TourList from './components/TourList/TourList';
 
